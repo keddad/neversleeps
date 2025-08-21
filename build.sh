8420f2820f29eee40d14fa85fe569c8078b44c83
Nickita Biryulin <keddad@yandex.ru>
2025-08-21 23:04:08 +0300

    Make it nicer

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ pandoc index.md -o index.html --standalone --template=templates/template.html
 # Build all publications
 for file in publications/*.md; do
   base=$(basename "$file" .md)
-  pandoc "$file" -o "publications/$base.html" --standalone --template=templates/template.html
+  pandoc "$file" -o "publications/$base.html" -f markdown-smart --standalone --template=templates/template.html
 done
--- index.md
+++ index.md
@@ -8,7 +8,3 @@ author: Nickita Biryulin
 * Software Engineer from Moscow, Russia
 * CV: [ru](publications/cv.html), [en](publications/cv_en.html)
 * I also take a lot of photos! [t.me/bronzesand](https://t.me/bronzesand)
-
-## Publications
-
-- [First Post](publications/demo.html)
